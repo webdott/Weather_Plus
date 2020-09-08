7c3c6a6ed94fe9da11c74f10b4891ad0f22f5d73
Caspero-62 <nwaforuchechukwu2007@gmail.com>
2020-09-08 14:10:07 +0100

    add service worker and finish up PWA

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,52 @@
-// const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v1';
 
-// //Add list of files to cache here.
-// const FILES_TO_CACHE = [
-//     '/offline.html',
-// ];
+//Add list of files to cache here.
+const FILES_TO_CACHE = [
+    '/offline.html',
+];
 
-// self.addEventListener('install', (evt) => {
-//     //Precache static resources here
-//     evt.waitUntil(
-//         caches.open(CACHE_NAME).then((cache) => {
-//             return cache.addAll(FILES_TO_CACHE);
-//         })
-//     );
+self.addEventListener('install', (evt) => {
+    //Precache static resources here
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
 
-//     self.skipWaiting();
-// })
+    self.skipWaiting();
+})
 
-// self.addEventListener('activate', (evt) => {
+self.addEventListener('activate', (evt) => {
     
 
-//    // Remove previous cached data from disk
-//    evt.waitUntil(
-//        caches.keys().then((keyList) => {
-//            return Promise.all(keyList.map((key) => {
-//                if (key !== CACHE_NAME) {
-//                    console.log('[ServiceWorker] Removing old cache', key);
-//                    return caches.delete(key);
-//                }
-//            }));
-//        })
-//    );
+   // Remove previous cached data from disk
+   evt.waitUntil(
+       caches.keys().then((keyList) => {
+           return Promise.all(keyList.map((key) => {
+               if (key !== CACHE_NAME) {
+                   console.log('[ServiceWorker] Removing old cache', key);
+                   return caches.delete(key);
+               }
+           }));
+       })
+   );
 
-//    self.clients.claim();
-// })
+   self.clients.claim();
+})
 
-// self.addEventListener('fetch', (evt) => {
-//     //Add  fetch event handler here
-//     if (evt.request.mode !== 'navigate') {
-//         //Not a page navigation, bail
-//         return;
-//     }
-//     evt.respondWith(
-//         fetch(evt.request)
-//             .catch(async () => {
-//                 const cache = await caches.open(CACHE_NAME);
-//                 return caches.match('offline.html');
-//             })
-//     );
-// });
+self.addEventListener('fetch', (evt) => {
+    //Add  fetch event handler here
+    if (evt.request.mode !== 'navigate') {
+        //Not a page navigation, bail
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+            .catch(async () => {
+                const cache = await caches.open(CACHE_NAME);
+                return caches.match('offline.html');
+            })
+    );
+});
 
 
